327a3b56eb43753a4530644053b9bf76df7a0720
Luiz Martinez <luizmar795@gmail.com>
2021-09-13 21:34:07 -0300

    Create e2e tests with cypress

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,16 @@
+// it('successfully logs in', () => {
+//     cy.intercept('GET', '**/notes').as('getNotes')
+//     cy.login()
+//     cy.wait('@getNotes')
+// })
+it('successfully logs in', () => {
+  cy.intercept('GET', '**/notes').as('getNotes')
+
+  cy.login(
+    Cypress.env('USER_EMAIL'),
+    Cypress.env('USER_PASSWORD'),
+    { cacheSession: false }
+  )
+
+  cy.wait('@getNotes')
+})
